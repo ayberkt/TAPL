fb27ba5c8279a89114e61bc09fe19d79048b80b5
Ayberk Tosun <ayberk.tosun@gmail.com>
2016-01-06 23:14:09 +0200

    Dummy REPL implementation for untyped

--- Untyped/Main.hs
+++ Untyped/Main.hs
@@ -2,5 +2,17 @@
 
 import Semantics
 
+import Control.Monad.Loops (whileM_)
+import System.IO (putStr, hFlush, getLine, stdout, hIsEOF, stdin)
+
 main ∷ IO ()
-main = putStrLn "Hi."
+main = do
+  putStr "λ "
+  hFlush stdout
+  whileM_ (fmap not $ hIsEOF stdin) $ do
+    hFlush stdout
+    input ← getLine
+    putStrLn $ "You typed " ++ input
+    putStr "λ "
+    hFlush stdout
+  putStrLn "Bye."
